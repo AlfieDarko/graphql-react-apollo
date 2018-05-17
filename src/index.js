2d49622713b8acc1b86cc8c969b3acfd2a9cff50
Alfie Darko <me@alfiedarko.co.uk>
2018-05-17 21:53:05 +0100

    updated LinkList.js to include Loading and Error messages

--- src/index.js
+++ src/index.js
@@ -4,37 +4,19 @@ import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-import {
-  ApolloProvider
-} from 'react-apollo'
+import {ApolloProvider} from 'react-apollo'
 
-import {
-  ApolloClient
-} from 'apollo-client'
+import {ApolloClient} from 'apollo-client'
 
-import {
-  HttpLink
-} from 'apollo-link-http'
+import {HttpLink} from 'apollo-link-http'
 
-import {
-  InMemoryCache
-} from 'apollo-cache-inmemory'
+import {InMemoryCache} from 'apollo-cache-inmemory'
 
-const httpLink = new HttpLink({
-  uri: 'http://localhost:4000'
-})
+const httpLink = new HttpLink({uri: 'http://localhost:4000'})
 
-const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
-})
+const client = new ApolloClient({link: httpLink, cache: new InMemoryCache()})
 
-ReactDOM.render( <
-    ApolloProvider client = {
-      client
-    } >
-    <
-    App / >
-    <
-    /ApolloProvider>, document.getElementById('root'));
+ReactDOM.render(< ApolloProvider client = {
+  client
+} > <App / > < /ApolloProvider>, document.getElementById('root'));
     registerServiceWorker();
